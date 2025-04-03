0f98e4abb69fa69acb3fab5c8b40be2b97ace84a
G992 <pvapvpa2014@yandex.ru>
2025-04-03 19:27:07 +0200

    chore: добавлен файл типов для поддержки Vue компонентов

---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" /> // Если бы мы использовали Vite, но оставим на всякий случай
+/// <reference types="vue/macros-global" />
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
+
+// Дополнительно можно объявить здесь глобальные типы, если они нужны,
+// например, типы для переменных окружения, если бы они использовались в коде клиента.
+// declare global {
+//   interface ImportMetaEnv {
+//     readonly VITE_APP_TITLE: string
+//     // more env variables...
+//   }
+//   interface ImportMeta {
+//     readonly env: ImportMetaEnv
+//   }
+// } 
