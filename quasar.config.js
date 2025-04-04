a7ae8a3e16185b058f0aee6bdf112d058fabeb5a
G992 <pvapvpa2014@yandex.ru>
2025-04-04 12:55:20 +0200

    chore: обновлены конфигурации сборки и иконки для поддержки Electron

--- quasar.config.js
+++ quasar.config.js
@@ -32,7 +32,6 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
       'pinia',
-      'router',
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
